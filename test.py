e7e02c9a82d85d68199b649fbe422df42ac5130e
Grace Tian <gracetian6@gmail.com>
2020-11-26 14:00:13 -0500

    random vertex/color

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # test.py
 from igraph import *
 import networkx as nx
+from random import choice
 
 # iGraph
 g = Graph()
@@ -9,9 +10,14 @@ g.add_vertices(3)
 
 # networkx
 G = nx.Graph()
-G.add_edge('A', 'B', weight=4)
-G.add_edge('B', 'D', weight=2)
-G.add_edge('A', 'C', weight=3)
-G.add_edge('C', 'D', weight=4)
-nx.shortest_path(G, 'A', 'D', weight='weight')
-print(nx.shortest_path(G, 'A', 'D', weight='weight'))
+G.add_edge(1, 2, weight=4)
+G.add_edge(2, 4, weight=2)
+G.add_edge(1, 3, weight=3)
+G.add_edge(3, 4, weight=4)
+nx.shortest_path(G, 1, 4, weight='weight')
+print(nx.shortest_path(G, 1, 4, weight='weight'))
+
+d = nx.coloring.greedy_color(G, strategy="largest_first")
+print(d)
+# random vertex
+print(choice(list(G.nodes())))
