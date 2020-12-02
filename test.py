86cbc7dbdcb2b04aa55c726e52894bbe0aefd6b1
Noah Singer <noahsinger@college.harvard.edu>
2020-12-02 16:37:55 -0500

    more cleanup

--- test.py
+++ test.py
@@ -4,20 +4,6 @@ import networkx as nx
 import color
 import matplotlib.pyplot as plt
 
-# networkx
-# G = nx.Graph()
-# G.add_edge(1, 2, weight=4)
-# G.add_edge(2, 4, weight=2)
-# G.add_edge(1, 3, weight=3)
-# G.add_edge(3, 4, weight=4)
-# nx.shortest_path(G, 1, 4, weight='weight')
-# print(nx.shortest_path(G, 1, 4, weight='weight'))
-
-# d = nx.coloring.greedy_color(G, strategy="largest_first")
-# print(d)
-# # random vertex
-# print(choice(list(G.nodes())))
-
 # pick an interesting small graph to try
 G = nx.tutte_graph()
 random_coloring = color.sample_coloring(G, 5)
