139c2bfeb4cde7466491e2c7bb85521e3faa6b73
Noah Singer <noahsinger@college.harvard.edu>
2020-12-02 11:22:16 -0500

    code to sample random coloring

--- color.py
+++ color.py
@@ -1,30 +1,61 @@
 import networkx as nx
 from random import choice
 
-class colorGraph:
-	def __init__(self, G):
-        # graph G, maybe set of colorings C?
-        self.G = G
+def random_vertex(G):
+    # return random vertex in graph
+    return choice(list(G.nodes()))
 
-	def randomVertex(self):
-        # return random vertex in graph
-        return choice(list(G.nodes()))
+def check_coloring_valid(G, C, v, c):
+    # look at all of v's neighbors
+    for neighbor in G[v]:
+        # is v already colored with c?
+        if v in C and C[v] == c:
+            return False
+    return True
 
-	def randomColor(self, C):
-        # return random color in C
-        return choice(list(C))
+# update a k-coloring to a random neighbor in-place
+def sample_adjacent_coloring(G, C, k):
+    v = random_vertex(G)
 
-    def validColor(self, G, v, c, C):
-        # check if vertex v with coloring c is valid coloring
-        # do this with BFS or DFS?
-        pass
+    # is the vertex already colored?
+    if v in C:
+        del C[v]
+    else:
+        # pick a random color
+        new_color = choice(list(range(k)))
+        # if the new coloring is OK, update it, else do nothing
+        if check_coloring_valid(G, C, v, new_color):
+            C[v] = new_color
 
-    def colorVertex(self, vertex, c):
-        # assign vertex color c
-        pass
 
-    def sampleColor(self, G, k, t):
-        pass
+# sample a k-coloring
+def sample_coloring(G, k, steps=1000, initial_C=None):
+    C = initial_C if initial_C else {}
+
+    for _ in range(steps):
+        sample_adjacent_coloring(G, C, k)
+
+    return C
+
+
+# class GraphColorer:
+# 	def __init__(self, G):
+#         # graph G, maybe set of colorings C?
+#         self.G = G
+
+# 	def random_color(self, C):
+#         # return random color in C
+#         return choice(list(C))
+
+#     def is_coloring_valid(self, G, v, c, C):
+#         pass
+
+#     def colorVertex(self, vertex, c):
+#         # assign vertex color c
+#         pass
+
+#     def sampleColor(self, G, k, t):
+#         pass
         # graph G (undirected, unweighted)
         # k : # of colors
         # t : # of steps
--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 # test.py
-from igraph import *
 import networkx as nx
 from random import choice
-
-# iGraph
-g = Graph()
-print(g)
-g.add_vertices(3)
+import color
 
 # networkx
-G = nx.Graph()
-G.add_edge(1, 2, weight=4)
-G.add_edge(2, 4, weight=2)
-G.add_edge(1, 3, weight=3)
-G.add_edge(3, 4, weight=4)
-nx.shortest_path(G, 1, 4, weight='weight')
-print(nx.shortest_path(G, 1, 4, weight='weight'))
+# G = nx.Graph()
+# G.add_edge(1, 2, weight=4)
+# G.add_edge(2, 4, weight=2)
+# G.add_edge(1, 3, weight=3)
+# G.add_edge(3, 4, weight=4)
+# nx.shortest_path(G, 1, 4, weight='weight')
+# print(nx.shortest_path(G, 1, 4, weight='weight'))
 
-d = nx.coloring.greedy_color(G, strategy="largest_first")
-print(d)
-# random vertex
-print(choice(list(G.nodes())))
+# d = nx.coloring.greedy_color(G, strategy="largest_first")
+# print(d)
+# # random vertex
+# print(choice(list(G.nodes())))
+
+# pick an interesting small graph to try
+G = nx.tutte_graph()
+random_coloring = color.sample_coloring(G, 5)
+print(random_coloring)
