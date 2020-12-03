80b732eec2bed689547eec5893db4fe87777cc82
Noah Singer <noahsinger@college.harvard.edu>
2020-12-03 12:24:43 -0500

    slow color enumeration code --- will try and optimize later

--- test.py
+++ test.py
@@ -5,11 +5,17 @@ import color
 import matplotlib.pyplot as plt
 
 # pick an interesting small graph to try
-G = nx.tutte_graph()
-random_coloring = color.sample_coloring(G, 5)
-print(random_coloring)
+G = nx.path_graph(10)
+# random_coloring = color.sample_coloring(G, 5)
+# print(random_coloring)
 
-list_of_colors = [(random_coloring[x] if x in random_coloring else 0) for x in range(G.number_of_nodes())]
+# list_of_colors = [(random_coloring[x] if x in random_coloring else 0) for x in range(G.number_of_nodes())]
 
-nx.draw(G, node_color=list_of_colors)
-plt.show()
+# nx.draw(G, node_color=list_of_colors)
+# plt.show()
+
+H = color.construct_graph_of_colorings(G, 4)
+print(len(H.nodes()))
+# nx.draw(H)
+# plt.show()
+# print(H)
